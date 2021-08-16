38c81037b28e6c029a1731011c13f3e03214c6ba
samandarkuchkarov <samandar8939522@gmail.com>
2021-08-16 20:56:25 +0500

    add

---
+++ pages/second.js
@@ -0,0 +1,11 @@
+export default function Second() {
+    return (
+      <div style={{width:'100vw',height:'100vh'}}>
+  
+        <video width="100%" height="100%" preload="auto" autoplay="true" loop="true" muted="muted" >
+            <source src="/diagram.mp4" type="video/mp4"/>
+        </video>
+  
+      </div>
+    )
+  }
